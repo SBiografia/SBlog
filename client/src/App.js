85b0ac74b9c4c81c5b97ba21d10fea39eb91b97f
SBiografia <lsb5870@gmail.com>
2021-08-10 23:48:36 +0900

    IE : add Banner

--- client/src/App.js
+++ client/src/App.js
@@ -8,13 +8,16 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./assets/custom.scss";
 const App = () => {
   const checkIE = () => {
-    const userAgent = window.navigator.userAgent.toLowerCase();
+    const userAgent = window.navigator.userAgent;
     console.log("start checkIE");
     if (userAgent.indexOf("Trident") > 0) {
+      alert("익스플로러에용");
       return true;
     } else if (/MSIE \d |Trident.*rv:/.test(userAgent)) {
+      alert("익스플로러에용");
       return true;
     } else {
+      alert("익스플로러 아니에요");
       return false;
     }
   };
